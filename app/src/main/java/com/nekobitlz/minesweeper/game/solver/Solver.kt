4d7e98e4543f077d64feb62b2f25a8586f67b6ff
Nekobitlz <jokers.bit@gmail.com>
2019-11-08 05:54:59 +0300

    Refactor solver & fix some bugs in it

--- app/src/main/java/com/nekobitlz/minesweeper/game/solver/Solver.kt
+++ app/src/main/java/com/nekobitlz/minesweeper/game/solver/Solver.kt
@@ -2,15 +2,18 @@ package com.nekobitlz.minesweeper.game.solver
 
 import android.util.Log
 import com.nekobitlz.minesweeper.game.enums.CellType
-import com.nekobitlz.minesweeper.game.enums.GameState.*
+import com.nekobitlz.minesweeper.game.enums.GameState.NO_STATE
 import com.nekobitlz.minesweeper.game.managers.CacheManager
 import com.nekobitlz.minesweeper.game.models.Cell
-import kotlin.collections.HashSet
+import kotlin.collections.Map.*
 import kotlin.random.Random
 
 class Solver {
 
     private val gameEngine = CacheManager.loadEngine()
+    private val width = CacheManager.COLUMN_COUNT
+    private val height = CacheManager.ROW_COUNT
+
     private var changed = true
     private var cellGroups = mutableSetOf<CellGroup>()
 
@@ -30,18 +33,20 @@ class Solver {
 
     private fun getCellGroups(): MutableSet<CellGroup> {
         val cellGroups = mutableSetOf<CellGroup>()
-        val openedCells = gameEngine.getCells().map { it.filter { cell -> cell.cellState.isOpened() && cell.cellType == CellType.COVERED } }
+        val openedCells = gameEngine.getCells().map {
+            it.filter { cell -> cell.cellState.isOpened() && cell.cellType == CellType.COVERED }
+        }
 
-        openedCells.forEach {
-            it.forEach { cell ->
-                val currentCellGroup = CellGroup(cell)
+        for (cellX in openedCells) {
+            for (cellY in cellX) {
+                val currentCellGroup = CellGroup(cellY)
 
                 if (currentCellGroup.isNotEmpty()) {
-                    cellGroups.forEach cellGroups@{ cellGroup ->
+                    cellGroups.forEach cellGroups@{
                         when {
-                            currentCellGroup == cellGroup -> return@cellGroups
-                            currentCellGroup.includes(cellGroup) -> currentCellGroup.subtract(cellGroup)
-                            cellGroup.includes(currentCellGroup) -> cellGroup.subtract(currentCellGroup)
+                            currentCellGroup == it -> return@cellGroups
+                            currentCellGroup.includes(it) -> currentCellGroup.subtract(it)
+                            it.includes(currentCellGroup) -> it.subtract(currentCellGroup)
                         }
                     }
 
@@ -54,51 +59,49 @@ class Solver {
     }
 
     private fun randomMove() {
-        val isolatedCells = getIsolatedCells()
+        val isolatedCells = gameEngine.board.isolatedClosedCells
         val isolatedCount = isolatedCells.size
 
         if (cellGroups.isEmpty() && isolatedCount == gameEngine.board.remainingFlags) {
             isolatedCells.forEach { cell -> gameEngine.handleLongPress(cell.x, cell.y) }
             Log.d("Solver", "randomMove: handleLongPress")
-        } else {
-            val cellCord = isolatedCells[getRandomIsolatedCell(isolatedCount)]
-            gameEngine.handleShortPress(cellCord.x, cellCord.y)
-            Log.d("Solver", "randomMove: handleShortPress ${cellCord.x} ${cellCord.y}")
+        }
+        else when (gameEngine.gameState) {
+            NO_STATE -> {
+                val x = Random(width).nextInt(width - 1)
+                val y = Random(height).nextInt(height - 1)
+
+                gameEngine.handleShortPress(x, y)
+            }
+            else -> {
+                val cellCord = isolatedCells[getRandomIsolatedCell(isolatedCount)]
+                gameEngine.handleShortPress(cellCord.x, cellCord.y)
+                Log.d("Solver", "randomMove: handleShortPress ${cellCord.x} ${cellCord.y}")
+            }
         }
 
         changed = isolatedCount > 0
         cellGroups = getCellGroups()
     }
 
-    private fun getIsolatedCells(): MutableList<Cell> {
-        val isolatedCells = mutableListOf<Cell>()
-        gameEngine.getCells().forEach { it.forEach { cell -> isolatedCells.add(cell) } }
-        val copy = isolatedCells.toList()
-
-        copy.map { cell ->
-            val neighbours = gameEngine.board.getNeighbours(cell)
-            isolatedCells.removeAll(neighbours)
-        }
-
-        return isolatedCells
-    }
-
     private fun getRandomIsolatedCell(isolatedCount: Int): Int {
         val random = Random(seed = isolatedCount)
 
-        return random.nextInt(isolatedCount + 1)
+        return random.nextInt(isolatedCount)
     }
 
     private fun subtractionMethod() {
-        cellGroups.forEach {
+        for (cellGroup in cellGroups) {
             when {
-                it.bombsCount == 0 -> {
-                    it.forEach { cell -> gameEngine.handleShortPress(cell.x, cell.y) }
+                cellGroup.bombsCount == 0 -> {
+                    cellGroup.forEach { cell -> gameEngine.handleShortPress(cell.x, cell.y) }
                     Log.d("Solver", "subtractionMethod: handleShortPress")
                     changed = true
                 }
-                it.bombsCount == it.size -> {
-                    it.forEach { cell -> if (!cell.cellState.isFlagged()) gameEngine.handleLongPress(cell.x, cell.y) }
+                cellGroup.bombsCount == cellGroup.size -> {
+                    cellGroup.forEach { cell ->
+                        if (!cell.cellState.isFlagged()) gameEngine.handleLongPress(cell.x, cell.y)
+                    }
                     Log.d("Solver", "subtractionMethod: handleLongPress")
                     changed = true
                 }
@@ -109,11 +112,21 @@ class Solver {
     }
 
     private fun probabilityMethod() {
+        val probabilities = initProbabilities()
+
+        val maxProbabilityEntry = probabilities.maxBy { it.value }!!
+        val minProbabilityEntry = probabilities.minBy { it.value }!!
+
+        openByRandom(minProbabilityEntry.value, maxProbabilityEntry.value)
+        openByProbability(minProbabilityEntry, maxProbabilityEntry)
+    }
+
+    private fun initProbabilities(): HashMap<Cell, Double> {
         val probabilities = hashMapOf<Cell, Double>()
 
-        cellGroups.forEach { cellGroup ->
-            if (cellGroup.size > 0) {
-                cellGroup.forEach { cell ->
+        for (cellGroup in cellGroups) {
+            if (cellGroup.isNotEmpty()) {
+                for (cell in cellGroup) {
                     val indieProbability = cellGroup.bombsCount / cellGroup.size.toDouble()
                     val oldProbability = if (probabilities[cell] != null) probabilities[cell]!! else 0.0
                     val newProbability = 1 - (1 - indieProbability) * (1 - oldProbability)
@@ -123,36 +136,41 @@ class Solver {
             }
         }
 
-        val maxProbabilityEntry = probabilities.maxBy { it.value }!!
-        val maxProbability = maxProbabilityEntry.value
-        val maxProbabilityCell = maxProbabilityEntry.key
+        return probabilities
+    }
 
-        val minProbabilityEntry = probabilities.minBy { it.value }!!
-        val minProbability = minProbabilityEntry.value
-        val minProbabilityCell = minProbabilityEntry.key
-
-        val isolatedCount = getIsolatedCells().size
+    private fun openByRandom(minProbability: Double, maxProbability: Double) {
+        val isolatedCount = gameEngine.board.isolatedClosedCells.size
 
         if (isolatedCount > 0) {
             var bombCellGroupCount = 0
 
-            cellGroups.forEach { bombCellGroupCount += it.bombsCount } //cellGroups.map { it.bombsCount }.count()
+            cellGroups.forEach { bombCellGroupCount += it.bombsCount }
 
             val randomOpenProbability = (gameEngine.board.remainingFlags - bombCellGroupCount) / isolatedCount.toDouble()
 
             if (randomOpenProbability < minProbability && randomOpenProbability < 1 - maxProbability) {
                 randomMove()
+                Log.d("Solver", "probabilityMethod: randomMove")
             }
         }
+    }
+
+    private fun openByProbability(minProbabilityEntry: Entry<Cell, Double>,
+                                  maxProbabilityEntry: Entry<Cell, Double>) {
+        val minProbability = minProbabilityEntry.value
+        val minProbabilityCell = minProbabilityEntry.key
+
+        val maxProbability = maxProbabilityEntry.value
+        val maxProbabilityCell = maxProbabilityEntry.key
 
         if (!changed) {
-            if (minProbability < 1 - maxProbability) {
-                if (minProbabilityCell.x in 0 until 9 && minProbabilityCell.y in 0 until 9) {
+            when {
+                minProbability < 1 - maxProbability && minProbabilityCell.x in 0 until width && minProbabilityCell.y in 0 until height -> {
                     gameEngine.handleShortPress(minProbabilityCell.x, minProbabilityCell.y)
                     Log.d("Solver", "probabilityMethod: handleShortPress")
                 }
-            } else {
-                if (maxProbabilityCell.x in 0 until 9 && maxProbabilityCell.y in 0 until 9) {
+                maxProbabilityCell.x in 0 until width && maxProbabilityCell.y in 0 until height -> {
                     gameEngine.handleLongPress(maxProbabilityCell.x, maxProbabilityCell.y)
                     Log.d("Solver", "probabilityMethod: handleLongPress")
                 }
@@ -160,31 +178,6 @@ class Solver {
 
             changed = true
             cellGroups = getCellGroups()
-        }
-    }
-
-    private inner class CellGroup(cell: Cell): HashSet<Cell>() {
-
-        internal var bombsCount = 0
-
-        init {
-            if (cell.cellState.isOpened()) {
-                val neighbours = gameEngine.board.getNeighbours(cell)
-                val openedNeighbours = neighbours.filter { !it.cellState.isOpened() && !it.cellState.isFlagged() }
-                val flaggedNeighbours = neighbours.filter { it.cellState.isFlagged() }.count()
-
-                addAll(openedNeighbours)
-                bombsCount = cell.bombsNearby - flaggedNeighbours
-            }
-        }
-
-        fun includes(cellGroup: CellGroup): Boolean {
-            return containsAll(cellGroup) && bombsCount >= cellGroup.bombsCount
-        }
-
-        fun subtract(cellGroup: CellGroup) = apply {
-            removeAll(cellGroup)
-            bombsCount -= cellGroup.bombsCount
         }
     }
 }
