92b06d5578bf506781670e3735ca54f2d86383b2
Nekobitlz <jokers.bit@gmail.com>
2019-11-10 02:14:01 +0300

    Transfer CellGroup from a separate file to Solver inner class

--- app/src/main/java/com/nekobitlz/minesweeper/game/solver/Solver.kt
+++ app/src/main/java/com/nekobitlz/minesweeper/game/solver/Solver.kt
@@ -195,4 +195,29 @@ class Solver {
             cellGroups = getCellGroups()
         }
     }
+
+    inner class CellGroup(cell: Cell): HashSet<Cell>() {
+
+        internal var bombsCount = 0
+
+        init {
+            if (cell.cellState.isOpened()) {
+                val neighbours = gameEngine.board.getNeighbours(cell)
+                val openedNeighbours = neighbours.filter { !it.cellState.isOpened() && !it.cellState.isFlagged() }
+                val flaggedNeighbours = neighbours.filter { it.cellState.isFlagged() }.count()
+
+                addAll(openedNeighbours)
+                bombsCount = cell.bombsNearby - flaggedNeighbours
+            }
+        }
+
+        fun includes(cellGroup: CellGroup): Boolean {
+            return containsAll(cellGroup) && bombsCount >= cellGroup.bombsCount
+        }
+
+        fun subtract(cellGroup: CellGroup) = apply {
+            removeAll(cellGroup)
+            bombsCount -= cellGroup.bombsCount
+        }
+    }
 }
